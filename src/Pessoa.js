12e845ad52b8a980a7a2b0ea3cbc936818b1a74a
Rian Fiore <74125852+Rianfiore@users.noreply.github.com>
2022-07-11 18:23:29 -0300

    first commit

---
+++ src/Pessoa.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+export class Pessoa extends Component {
+  state = {
+    nome: "Rian",
+    idade: 22,
+    comida_favorita: "Churrasco",
+    musicas: [
+      {
+        nome: "Carry On",
+        banda: "Angra",
+      },
+      {
+        nome: "Tenho Medo",
+        banda: "Zé Vaqueiro",
+      },
+      {
+        nome: "I See Fire",
+        banda: "Feuerschwanz",
+      },
+    ],
+  };
+
+  render() {
+    return (
+      <>
+        <h1>{`Meu nome é ${this.state.nome}`}</h1>
+        <h2>{`Eu tenho ${this.state.idade} anos de idade`}</h2>
+        <h3>{`Minha comida favorita é ${this.state.comida_favorita}`}</h3>
+        <ul>
+          {this.state.musicas.map((musica, index) => {
+            return (
+              <li>
+                Musica {index + 1}
+                <ul>{`Nome da música: ${musica.nome}`}</ul>
+                <ul>{`Nome da banda: ${musica.banda}`}</ul>
+              </li>
+            );
+          })}
+        </ul>
+      </>
+    );
+  }
+}
